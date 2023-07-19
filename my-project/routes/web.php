41dd8202b3fe77270eb1aaa9a1dd84ae48410fa4
livialayane <liviabrlayane@gmail.com>
2023-07-19 11:52:01 -0300

    web.php

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Counter;
+use App\Http\Livewire\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/contador', Counter::class, function () { 
+    
+});
+Route::get('/index', Index::class, function () { 
+    
 });
